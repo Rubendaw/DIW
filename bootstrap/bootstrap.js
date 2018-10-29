48edc5e2e64425b7ff6ed83f062ee9f3c403f335
Rubendaw <43141558+Rubendaw@users.noreply.github.com>
2018-10-29 17:24:21 +0100

    Update bootstrap.js

--- bootstrap/bootstrap.js
+++ bootstrap/bootstrap.js
@@ -4,3 +4,36 @@ function onSubmit( form ){
     console.log( data );
     return false; //don't submit
   }
+
+  function showContent() {
+
+    var appBanners = document.getElementsByClassName('grave'), i;
+ 
+    var appBanners2 = document.getElementsByClassName('leve'), j;
+ 
+    var check = document.getElementById("check");
+ 
+    if (check.checked==true) {
+     for (var i = 0; i < appBanners.length; i ++) {
+       appBanners[i].style.display = 'block';
+ 
+   }
+ 
+   for (var j = 0; j < appBanners2.length; j ++) {
+     appBanners2[j].style.display = 'none';
+ 
+ }
+ 
+    }
+    else {
+     for (var i = 0; i < appBanners.length; i ++) {
+       appBanners[i].style.display = 'none';
+     }
+ 
+     for (var j = 0; j < appBanners2.length; j ++) {
+       appBanners2[j].style.display = 'block';
+     }
+    }
+ 
+    
+ }
