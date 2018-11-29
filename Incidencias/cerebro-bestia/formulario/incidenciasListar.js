4944250c243ad392c94be2a6dfe28c4ec7b0e6bc
Ruben Montejano <rubenmontejano2@gmail.com>
2018-11-29 16:10:01 +0100

    hola que tal estamos

--- Incidencias/cerebro-bestia/formulario/incidenciasListar.js
+++ Incidencias/cerebro-bestia/formulario/incidenciasListar.js
@@ -2,7 +2,7 @@ $.getJSON( "incidencias", function( data ) {
   var items = [];
     $.each( data, function( key, val ) {
 	console.log(key);
-    items.push( "<li id='" + key + "' class='list-group-item'>" + val.alumne + val.grupo + "</li>" );
+    items.push( "<li id='" + key + "' class='list-group-item'>" + val.alumne + " " + val.grupo + " " + val.profesor + " " + val.data + "</li>" );
   });
  
   $( "<ul/>", {
